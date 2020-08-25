2ffbc3a348f8836826f8a02a1e47c63f3649769a
clwater <gengzhibo@gengzhibo.net>
2020-08-26 07:06:10 +0800

    feat(tools): 增加自动生成文档表格内容工具

---
+++ tools/helper.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+tag=$1
+echo "auto help $tag"
+
+if [[ $tag == '' ]]; then
+     echo 'tag is empty'
+     exit
+fi
+
+
+folder=`find . -name $tag*`
+
+folder=${folder/.\//}
+
+cd $folder
+gif=`find . -name $tag*_small.gif`
+txt="|[$folder](/$folder/code.md)|$tag|-|![](/$folder/$gif)|&check;|"
+
+echo $txt
+# 现只支持mac的自动复制到粘贴板
+echo $txt | pbcopy
